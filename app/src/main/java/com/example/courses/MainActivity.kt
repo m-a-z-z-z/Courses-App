ab6883c2eedf87748d5f3631e9a1ba8dac5e2042
Dylan Maziarek <21125921+m-a-z-z-z@users.noreply.github.com>
2024-06-03 13:45:40 +0100

    Cards finished and arranged in grid

--- app/src/main/java/com/example/courses/MainActivity.kt
+++ app/src/main/java/com/example/courses/MainActivity.kt
@@ -4,13 +4,40 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.courses.data.DataSource
+import com.example.courses.model.Topic
 import com.example.courses.ui.theme.CoursesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +46,54 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CoursesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CoursesApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CoursesApp() {
+    TopicList(topicList = DataSource.topics)
+}
+
+@Composable
+fun CourseCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Row {
+            Image(
+                painter = painterResource(id = topic.imageResourceId),
+                contentDescription = stringResource(id = topic.stringResourceId),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(68.dp)
+                    .aspectRatio(1f)    // aspect ratio must be after size or it ignores size
+
+            );
+            Column {
+                Text(
+                    text = stringResource(id = topic.stringResourceId),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 8.dp),
+                    onTextLayout = { null as ((TextLayoutResult) -> Unit)? }
+                );
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = "grain icon",
+                        modifier = Modifier.padding(start = 16.dp, end = 8.dp)
+                    );
+                    Text(
+                        text = topic.integerResourceId.toString(),
+                        style = MaterialTheme.typography.labelMedium,
+                        onTextLayout = { null as ((TextLayoutResult) -> Unit)? }
                     )
                 }
             }
@@ -31,17 +102,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TopicList(topicList: List<Topic>, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(columns = GridCells.Fixed(2), modifier = Modifier) {
+        items(topicList) { topic ->
+            CourseCard(topic)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CoursesPreview(modifier: Modifier = Modifier) {
     CoursesTheme {
-        Greeting("Android")
+        TopicList(topicList = DataSource.topics,
+            modifier = Modifier.padding(8.dp)
+        )
     }
 }
--- app/src/main/java/com/example/courses/model/Topic.kt
+++ app/src/main/java/com/example/courses/model/Topic.kt
@@ -1,4 +1,11 @@
 package com.example.courses.model
 
-class Topic {
-}
+import androidx.annotation.DrawableRes
+import androidx.annotation.IntegerRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @StringRes val stringResourceId: Int,
+    val integerResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
